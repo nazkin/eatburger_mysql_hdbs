5e01e06387fb762c311264ceffc2694b7aa9242a
nazkin <nazar.kinash93@gmail.com>
2020-02-05 20:22:03 -0500

    failed deployment

--- config/connection.js
+++ config/connection.js
@@ -1,44 +1,22 @@
 const mysql = require('mysql');
 
 
-// const connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "19935ert7",
-//     database: "burgers_db"
-// });
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
-const dbConfig = {
-  HOST: "us-cdbr-iron-east-04.cleardb.net",
-  USER: "bd6996868c6eef",
-  CONNECTION: "9efc0cb3",
-  DB: "heroku_1bef034957a67d5"
-}
-
-const connection = mysql.createPool({
-  host: dbConfig.HOST, 
-  user: dbConfig.USER,
-  password: dbConfig.CONNECTION,
-  database: dbConfig.DB
-})
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "19935ert7",
+    database: "burgers_db"
+});
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
-  
 
 module.exports = connection;
 
--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,9 @@ CREATE DATABASE burgers_db ;
 USE burgers_db;
 
 CREATE TABLE burgers (
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   burger_name VARCHAR(200) NULL,
   devoured BOOLEAN,
   PRIMARY KEY (id)
 );
+
