05982806423990a73375b1af0f8b6fb7a9e1f6fe
nazkin <nazar.kinash93@gmail.com>
2020-02-05 19:30:19 -0500

    heroku db config

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,30 @@
 const mysql = require('mysql');
 
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "19935ert7",
-    database: "burgers_db"
-});
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "19935ert7",
+//     database: "burgers_db"
+// });
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
-connection.connect(function(err) {
+const connection = mysql.createPool({
+  host: "us-cdbr-iron-east-04.cleardb.net",
+  user: "bd6996868c6eef",
+  connection: "9efc0cb3",
+  database: "heroku_1bef034957a67d5"
+})
+
+
+  connection.connect(function(err) {
     if (err) {
       console.error("error connecting: " + err.stack);
       return;
@@ -20,3 +35,9 @@ connection.connect(function(err) {
 
 module.exports = connection;
 
+// {
+//   username:bd6996868c6eef,
+//   password: 9efc0cb3, 
+//   host: us-cdbr-iron-east-04.cleardb.net,
+//   database: heroku_1bef034957a67d5
+// }
