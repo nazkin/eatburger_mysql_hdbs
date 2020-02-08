b1211d6ccb4cd44072121b2583b6228b7793e9eb
nazkin <nazar.kinash93@gmail.com>
2020-02-08 00:36:41 -0500

    Git deployment

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE burgers_db ;
 USE burgers_db;
 
 CREATE TABLE burgers (
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   burger_name VARCHAR(200) NULL,
   devoured BOOLEAN,
   PRIMARY KEY (id)
